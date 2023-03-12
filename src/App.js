1515c538e8ed7f970c3f9780f09deb9f437a8d7c
shubhamnimje77@gmail.com <shubhamnimje77@gmail.com>
2023-03-12 17:46:42 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,26 @@ import Demooutput from './components/Demo/Demooutput';
 
 function App() {
   const [ShowPara, SetShowPara] = useState(false);
+  const [AllowToggler, SetAllowToggler] = useState(false);
+
 
   console.log('APP RUNNING');
 
   const ToogleParaHandler = useCallback(() => {
+    if(AllowToggler) {
     SetShowPara(preShowPara => !preShowPara);
-  },[]);
-
+    }
+  },[AllowToggler]);
+  
+  const AllowToggleHandler = () => {
+    SetAllowToggler(true);
+  }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <Demooutput show={false}/>
+      <Demooutput show={ShowPara}/>
+      <Button onClick={AllowToggleHandler}>Allow Toggle</Button>
       <Button onClick={ToogleParaHandler}>Toggle Paragraph</Button>
     </div>
   );
